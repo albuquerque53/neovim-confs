3eb8c3ac491f5e2aa6617c20bef27c0989f0be2a
Gabriel Albuquerque <albuquerque.se@proton.me>
2023-04-26 11:42:29 -0300

    chore: bufferline confs and maps

--- lua/bufferline.rc.lua
+++ lua/bufferline.rc.lua
@@ -3,10 +3,12 @@ vim.opt.termguicolors = true
 require("bufferline").setup{
     options = {
         mode = tabs,
-        separator_style = 'slant',
+        separator_style = 'thick',
         always_show_bufferline = false,
-        show_buffer_close_icons = false,
         show_close_icon = false,
         color_icons = true
     }
 }
+
+vim.api.nvim_set_keymap('n', 'gt', '<cmd>BufferLineCycleNext<CR>', {})
+vim.api.nvim_set_keymap('n', 'gT', '<cmd>BufferLineCyclePrev<CR>', {})
