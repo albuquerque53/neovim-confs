79c72e48ff4443796e9c52a0cc0f255b03ba50c2
Gabriel Albuquerque <albuquerque.se@proton.me>
2023-05-09 17:05:49 -0300

    refactor: rewrite init.vim in lua

---
+++ init.lua
@@ -0,0 +1,54 @@
+vim.cmd('filetype plugin indent on')
+
+-- Plugins
+require('plugins')
+
+-- Editor
+vim.opt.cursorline = true
+vim.opt.hidden = true
+vim.opt.number = true
+vim.opt.autoindent = true
+vim.opt.relativenumber = true
+vim.opt.inccommand = 'split'
+vim.opt.modifiable = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 0
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.mouse = 'a'
+require('config.telescope')
+require('config.filebrowser')
+require('config.gitsigns')
+require('config.git')
+require('config.bufferline')
+require('config.fzf')
+require('config.indentlines')
+
+-- LSP
+require('config.lspconfig')
+require('config.lspsaga')
+require('config.lspkind')
+require('config.cmp')
+require('config.treesiter')
+require('config.autopairs')
+vim.opt.omnifunc = 'syntaxcomplete#Complete'
+
+-- Theme
+require('config.lualine')
+vim.cmd('colorscheme melange')
+vim.opt.background = 'dark'
+vim.cmd('highlight Normal ctermbg=NONE guibg=NONE')
+
+-- Keymaps 
+vim.g.mapleader = '\\'
+vim.api.nvim_set_keymap('n', '<leader>cl', ':bd<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>fcs', ':vsplit ~/.config/nvim/init.lua<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>fce', ':edit ~/.config/nvim/init.lua<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>fcl', ':source ~/.config/nvim/init.lua<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader><space>', ':nohlsearch<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>pi', ':PackerInstall<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>ff', ':Telescope find_files<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>fb', ':Telescope buffers<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>fh', ':Telescope help_tags<cr>', { noremap = true })
