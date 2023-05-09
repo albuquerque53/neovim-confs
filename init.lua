193c91a748226f1477b875ee8a2c323855164b1d
Gabriel Albuquerque <albuquerque.se@proton.me>
2023-05-09 19:16:54 -0300

    chore: packer usage improvement

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 vim.cmd('filetype plugin indent on')
 
 -- Plugins
-require('lua.plugins')
+require('plugins')
 
 -- Editor
 vim.opt.cursorline = true
@@ -44,6 +44,8 @@ vim.g.mapleader = '\\'
 vim.api.nvim_set_keymap('n', '<leader>cl', ':bd<cr>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>fcs', ':vsplit ~/.config/nvim/init.lua<cr>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>fce', ':edit ~/.config/nvim/init.lua<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>fpe', ':edit ~/.config/nvim/lua/plugins.lua<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>fps', ':edit ~/.config/nvim/lua/plugins.lua<cr>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>fcl', ':source ~/.config/nvim/init.lua<cr>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader><space>', ':nohlsearch<cr>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>pi', ':PackerInstall<cr>', { noremap = true })
