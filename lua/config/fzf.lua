f2ef11d2e22d93078d8b2ef36f4b487406ff7b15
Gabriel Albuquerque <albuquerque.se@proton.me>
2023-05-09 19:13:06 -0300

    feat: using fzf to navigate code base

--- lua/config/fzf.lua
+++ lua/config/fzf.lua
@@ -1,1 +1,2 @@
 vim.keymap.set("n", "<leader>fg", "<cmd>lua require('fzf-lua').live_grep()<CR>", { silent = true })
+vim.keymap.set("n", "<leader>ff", "<cmd>lua require('fzf-lua').files()<CR>", { silent = true })
