58a1f4a674b48ecc44bd4452a7d9c494b0ed6eed
Gabriel Albuquerque <albuquerque.se@proton.me>
2023-05-10 20:57:02 -0300

    refactor: restructuring plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,10 +2,24 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
+
+    -- lsp 
     use 'neovim/nvim-lspconfig'
-    use "savq/melange-nvim"
-    use "lukas-reineke/indent-blankline.nvim"
     use 'L3MON4D3/LuaSnip'
+    use({
+        'glepnir/lspsaga.nvim',
+        opt = true,
+        branch = 'main',
+        event = 'LspAttach',
+        config = function()
+            require('lspsaga').setup({})
+        end,
+        requires = {
+            {'nvim-tree/nvim-web-devicons'},
+        }
+    })
+    use 'folke/lsp-colors.nvim'
+    use 'onsails/lspkind.nvim'
     use {
         'nvim-treesitter/nvim-treesitter',
         run = function()
@@ -13,41 +27,33 @@ return require('packer').startup(function(use)
             ts_update()
         end,
     }
-    use({
-        "glepnir/lspsaga.nvim",
-        opt = true,
-        branch = "main",
-        event = "LspAttach",
-        config = function()
-            require("lspsaga").setup({})
-        end,
-        requires = {
-            {"nvim-tree/nvim-web-devicons"},
-        }
-    })
-    use 'folke/lsp-colors.nvim'
-    use 'onsails/lspkind.nvim'
+
+    -- appearance 
+    use 'ryanoasis/vim-devicons'
+    use 'savq/melange-nvim'
+    use 'lukas-reineke/indent-blankline.nvim'
+
+    -- editor
+    use 'ggandor/lightspeed.nvim'
+    use { 'junegunn/fzf', run = './install --bin', }
+    use { 'ibhagwan/fzf-lua',
+      requires = { 'nvim-tree/nvim-web-devicons' }
+    }
+    use {
+      'windwp/nvim-autopairs',
+      config = function() require('nvim-autopairs').setup {} end
+    }
     use {
       'nvim-lualine/lualine.nvim',
       requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
     use {
-      "windwp/nvim-autopairs",
-      config = function() require("nvim-autopairs").setup {} end
-    }   
-    use 'lewis6991/gitsigns.nvim'
-    use {
-      'dinhhuy258/git.nvim'
-    }
-    use {
         'akinsho/bufferline.nvim',
-        tag = "*",
+        tag = '*',
         requires = 'nvim-tree/nvim-web-devicons'
     }
-    use 'ryanoasis/vim-devicons'
-    use { 'junegunn/fzf', run = './install --bin', }
-    use { 'ibhagwan/fzf-lua',
-      requires = { 'nvim-tree/nvim-web-devicons' }
-    }
-    use 'ggandor/lightspeed.nvim'
+    
+    -- git
+    use 'lewis6991/gitsigns.nvim'
+    use 'dinhhuy258/git.nvim'
 end)
